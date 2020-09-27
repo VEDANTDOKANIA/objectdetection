48865041461af3960addc2ab171d54393433fcf9
VEDANTDOKANIA <61377008+VEDANTDOKANIA@users.noreply.github.com>
2020-09-27 12:21:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(FlutterVisionApp());
+}
+
+class FlutterVisionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Object Detection',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String imgPath;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text("Flutter ml vision"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                height: 150,
+                child: Container(
+                  height: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Center(
+                      child: _imagelist(),
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _imagelist() {
+    return GestureDetector(
+      child: Center(
+        child: Image.asset("lib/assets/ironman.jpg"),
+      ),
+      onTap: () async {
+        print("Trying to detect Image");
+        String file = await getImageFileFromAssets("ironman.jpg");
+        print(file);
+        setState(() {
+          imgPath = file;
+        });
+        detectLabels();
+      },
+    );
+  }
+
+  void showInSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  Future<String> getImageFileFromAssets(String path) async {
+    final bytedata = await rootBundle.load("lib/assets/$path");
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/pictures/fluttervision';
+    await Directory(dirPath).create(recursive: true);
+    final String filePath =
+        '$dirPath/${DateTime.now().microsecondsSinceEpoch.toString()}.jpg';
+    final file = File(filePath);
+    await file.writeAsBytes(bytedata.buffer
+        .asUint8List(bytedata.offsetInBytes, bytedata.lengthInBytes));
+    return filePath;
+  }
+
+  Future<void> detectLabels() async {
+    final ImageLabeler cloudLabeler =
+        FirebaseVision.instance.cloudImageLabeler();
+    final FirebaseVisionImage visionImage =
+        FirebaseVisionImage.fromFilePath(imgPath);
+    final ImageLabeler labelDetector = FirebaseVision.instance
+        .imageLabeler(ImageLabelerOptions(confidenceThreshold: 0.25));
+    final List<ImageLabel> labels =
+        await labelDetector.processImage(visionImage);
+    final List<ImageLabel> cloudLabels =
+        await cloudLabeler.processImage(visionImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.text;
+      final String entityId = label.entityId;
+      final double confidence = label.confidence;
+      print(text);
+      print(confidence);
+      print(entityId);
+      _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(text)));
+    }
+  }
+}
